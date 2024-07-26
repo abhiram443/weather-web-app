8eba68c4eaaef31b57db6da979f88c8d6516373b
abhiram443 <abhisattineedi@gmail.com>
2024-07-26 13:49:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather from './components/weather'
+
+function App() {
+
+
+  return (
+    <div >
+        <Weather/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './weather.css'
+
+const weather = () => {
+  return (
+    <div>
+      Weather app
+    </div>
+  )
+}
+
+export default weather
