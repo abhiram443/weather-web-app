27e2524be6c39db9e1fbb6d12c3bdae941c0d21f
abhiram443 <abhisattineedi@gmail.com>
2024-07-27 20:16:44 +0530

    Completed and fixed bugs

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,10 +1,129 @@
-import React from 'react'
+import React,{useEffect, useRef, useState} from 'react'
 import './weather.css'
+import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import humidity_icon from '../assets/humidity.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import wind_icon from '../assets/wind.png'
 
 const weather = () => {
+  
+
+
+  const inputRef = useRef()
+  const [weatherData,setweatherData]=useState(false);
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  }
+
+
+
+
+
+
+  const search = async(city) =>{
+    if(city===""){
+      alert("Please enter a city name");
+      return;
+
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+      if(!response.ok){
+        alert("city not found");
+        return;
+      }
+
+      console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setweatherData(
+        {
+          humidity: data.main.humidity,
+          windSpeed: data.wind.speed,
+          temperature: Math.floor(data.main.temp),
+          location: data.name,
+          icon: icon
+        }
+      )
+
+    } catch (error) {
+      setweatherData(false);
+      console.error("Unable to fetch weather Data");
+
+      
+    }
+  }
+  useEffect(()=>{
+    search("London");
+  },[])
+
+
+
+
+
+
+
+
+
+
   return (
-    <div>
-      Weather app
+    <div className='outer'>
+      <div className='weatherBox'>
+        <div className='search-container'>
+          <input ref={inputRef} className='searchbar' type='text' placeholder='Search'></input>
+          <img src={search_icon} onClick={()=> search(inputRef.current.value)}></img>
+
+        </div>
+        {weatherData?<>
+        <div className='icon-container'>
+          <img src={weatherData.icon} className='weather-icon'></img>
+          <p className='temperature'>{weatherData.temperature}°c</p>
+          <p className='location'>{weatherData.location}</p>
+
+        </div>
+        <div className='footer-container'>
+          <div className='info-container'>
+            <img src={humidity_icon}></img>
+            <p>{weatherData.humidity}%</p>
+            <span>Humidity</span>
+
+          </div>
+          <div className='info-container'>
+          <img src={wind_icon}></img>
+            <p>{weatherData.windSpeed}Km/Hr</p>
+            <span>Wind Speed</span>
+
+
+
+          </div>
+
+        </div>
+        
+        </>:<></>}
+        
+      
+
+      </div>
+      
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/app.jsx","./src/components/weather.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
